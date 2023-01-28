04d8f2a37dec0eaf5e4c26e8762a5b98da7dcafc
mumz0 <parisot.sa@gmail.com>
2023-01-28 18:24:50 +0100

    review done

--- front/js/product.js
+++ front/js/product.js
@@ -3,62 +3,62 @@
  * to product page. handle adding item to cart on click event
  */
 function getProductDetailsAndDisplay() {
-    id = getIdFromUrl("_id");
-    getProductById(id)
-        .then((res) => res.json())
-        .then((item) => {
-            displayKanapDetails(item);
-            addClickEvent(item);
-        });
+  id = getIdFromUrl("_id");
+  getProductById(id)
+      .then((res) => res.json())
+      .then((item) => {
+          displayKanapDetails(item);
+          addClickEvent(item);
+      });
 }
 
 /**
- * Display product details in DOM
- * @param {{}} itemDetails 
- */
+* Display product details in DOM
+* @param {{}} itemDetails 
+*/
 function displayKanapDetails(itemDetails) {
-    let imageUrl = document.querySelector(".item__img");
-    imageUrl.innerHTML = `<img src="${itemDetails.imageUrl}" alt="${itemDetails.altTxt}">`;
-    let name = document.getElementById("title");
-    name.innerHTML = itemDetails.name;
-    let price = document.getElementById("price");
-    price.innerHTML = itemDetails.price;
-    let description = document.getElementById("description");
-    description.innerHTML = itemDetails.description;
-    let color = document.getElementById("colors");
-    for (index = 0; index < itemDetails.colors.length; index++) {
-        color.innerHTML += `<option value="${itemDetails.colors[index]}">${itemDetails.colors[index]}</option>`;
-    }
+  let imageUrl = document.querySelector(".item__img");
+  imageUrl.innerHTML = `<img src="${itemDetails.imageUrl}" alt="${itemDetails.altTxt}">`;
+  let name = document.getElementById("title");
+  name.innerHTML = itemDetails.name;
+  let price = document.getElementById("price");
+  price.innerHTML = itemDetails.price;
+  let description = document.getElementById("description");
+  description.innerHTML = itemDetails.description;
+  let color = document.getElementById("colors");
+  for (index = 0; index < itemDetails.colors.length; index++) {
+      color.innerHTML += `<option value="${itemDetails.colors[index]}">${itemDetails.colors[index]}</option>`;
+  }
 }
 
 
 /**
- * Get quantity and color selected, create Cartitem object and add it to cart only if
- * a color and a quantity not over 100 has been selected. Else an alert has been send to user. 
- * @param {{}} item 
- */
+* Get quantity and color selected, create Cartitem object and add it to cart only if
+* a color and a quantity not over 100 has been selected. Else an alert has been send to user. 
+* @param {{}} item 
+*/
 function addItem(item) {
-    const _quantity = Number(document.getElementById("quantity").value);
-    const _color = document.getElementById("colors").value;
-    if (_color != "") {
-        let _item = new CartItem(item._id, _color, _quantity);
-        addItemToCart(_item);
-    } else {
-        alert("Veuillez choisir une couleur")
-    }
-    if (_quantity > 100) {
-        alert("Veuillez choisir une quantité comprise entre 1 et 100");
-    }
+  const _quantity = Number(document.getElementById("quantity").value);
+  const _color = document.getElementById("colors").value;
+  if (_color != "") {
+      let _item = new CartItem(item._id, _color, _quantity);
+      addItemToCart(_item);
+  } else {
+      alert("Veuillez choisir une couleur")
+  }
+  if (_quantity > 100) {
+      alert("Veuillez choisir une quantité comprise entre 1 et 100");
+  }
 }
 
 
 /**
- * Get Add to cart button element and call addItem function on click.
- * @param {{}} item 
- */
+* Get Add to cart button element and call addItem function on click.
+* @param {{}} item 
+*/
 function addClickEvent(item) {
-    const CartButton = document.getElementById("addToCart");
-    CartButton.addEventListener("click", (event) => addItem(item, event));
+  const CartButton = document.getElementById("addToCart");
+  CartButton.addEventListener("click", (event) => addItem(item, event));
 }
 
 getProductDetailsAndDisplay();
--- front/js/shared.js
+++ front/js/shared.js
@@ -1,77 +1,74 @@
 class CartItem {
-    constructor(id, color, quantity) {
-        this.id = id;
-        this.color = color;
-        this.quantity = quantity;
-    }
+  constructor(id, color, quantity) {
+      this.id = id;
+      this.color = color;
+      this.quantity = quantity;
+  }
 }
 
 const cartKey = "CartItems";
 
 /**
- * Get ID product from URL
- * @param {String} param 
- * @returns String
- */
+* Get ID product from URL
+* @param {String} param 
+* @returns String
+*/
 function getIdFromUrl(param) {
-    return new URL(location).searchParams.get(param);
+  return new URL(location).searchParams.get(param);
 }
 
 
 /**
- * Get product object by ID
- * @param {String} id 
- * @returns Promise | null
- */
+* Get product object by ID
+* @param {String} id 
+* @returns Promise | null
+*/
 function getProductById(id) {
-    if (id) {
-        return fetch("http://localhost:3000/api/products/" + id);
-    } else {
-        return null;
-    }
+  if (id) {
+      return fetch("http://localhost:3000/api/products/" + id);
+  } else {
+      return null;
+  }
 }
 
 
 /**
- * Get cart from localstorage and create it if doesn't exist
- * @returns {CartItem[]} List of item
- */
+* Get cart from localstorage and create it if doesn't exist
+* @returns {CartItem[]} List of item
+*/
 function getCart() {
-    let arrayString = localStorage.getItem(cartKey);
-    if (arrayString != null) {
-        return JSON.parse(arrayString);
-    } else {
+  let arrayString = localStorage.getItem(cartKey);
+  if (arrayString != null) {
+      return JSON.parse(arrayString);
+  } else {
 
-        return [];
-    }
+      return [];
+  }
 }
 
 /**
- * Write cart to localStorage
- * @param {[*]} cart 
- */
+* Write cart to localStorage
+* @param {[*]} cart 
+*/
 function writeCart(cart) {
-    localStorage.setItem(cartKey, JSON.stringify(cart));
+  localStorage.setItem(cartKey, JSON.stringify(cart));
 }
 
 
 /**
- * Get item list in localStorage, find item in list and add quantity selected if 
- * item is already in list. Else add item to list and push to localstorage in string format.
- * @param {CartItem} addedItem 
- */
+* Get item list in localStorage, find item in list and add quantity selected if 
+* item is already in list. Else add item to list and push to localstorage in string format.
+* @param {CartItem} addedItem 
+*/
 function addItemToCart(addedItem) {
-    let cart = getCart();
-    let oldItem = cart.find(
-        (item) => item.id === addedItem.id && item.color === addedItem.color
-    );
-    if (oldItem) {
-        oldItem.quantity += addedItem.quantity;
-    } else {
-        cart.push(addedItem);
-    }
-    writeCart(cart);
+  let cart = getCart();
+  let oldItem = cart.find(
+      (item) => item.id === addedItem.id && item.color === addedItem.color
+  );
+  if (oldItem) {
+      oldItem.quantity += addedItem.quantity;
+  } else {
+      cart.push(addedItem);
+  }
+  writeCart(cart);
 }
-
-
-
